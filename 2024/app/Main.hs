d4dda3a2141fee4adfcafd438dea689208044782
Aleksandr Matrosov <matroskin20906@gmail.com>
2024-12-10 18:48:52 +0200

    feat: day 10 solution.

--- 2024/app/Main.hs
+++ 2024/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Day1.Solution as S1
+import Day10.Solution as S10
 import Day2.Solution as S2
 import Day3.Solution as S3
 import Day4.Solution as S4
@@ -12,21 +13,33 @@ import Day9.Solution as S9
 
 main :: IO ()
 main = do
+  print "day 1:"
   S1.firstPart "inputs/input1.txt"
   S1.secondPart "inputs/input1.txt"
+  print "day 2:"
   S2.firstPart "inputs/input2.txt"
   S2.secondPart "inputs/input2.txt"
+  print "day 3:"
   S3.firstPart "inputs/input3.txt"
   S3.secondPart "inputs/input3.txt"
+  print "day 4:"
   S4.firstPart "inputs/input4.txt"
   S4.secondPart "inputs/input4.txt"
+  print "day 5:"
   S5.firstPart "inputs/input5.txt"
   S5.secondPart "inputs/input5.txt"
+  print "day 6:"
   S6.firstPart "inputs/input6.test"
   S6.secondPart "inputs/input6.test"
+  print "day 7:"
   S7.firstPart "inputs/input7.txt"
   S7.secondPart "inputs/input7.txt"
+  print "day 8:"
   S8.firstPart "inputs/input8.txt"
   S8.secondPart "inputs/input8.txt"
+  print "day 9:"
   S9.firstPart "inputs/input9.test"
   S9.secondPart "inputs/input9.test"
+  print "day 10:"
+  S10.firstPart "inputs/input10.txt"
+  S10.secondPart "inputs/input10.txt"
---
+++ 2024/src/Day10/Solution.hs
@@ -0,0 +1,101 @@
+module Day10.Solution
+  ( firstPart,
+    secondPart,
+  )
+where
+
+import Data.Char (digitToInt)
+import Data.Set (Set, fromList)
+import Prelude hiding (Left, Right)
+
+data Movement = Up | Down | Right | Left
+
+toVector :: Movement -> (Int, Int)
+toVector Up = (0, -1)
+toVector Down = (0, 1)
+toVector Right = (1, 0)
+toVector Left = (-1, 0)
+
+sumVectors :: (Int, Int) -> (Int, Int) -> (Int, Int)
+sumVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+sumVectorMovement :: (Int, Int) -> Movement -> (Int, Int)
+sumVectorMovement v m = sumVectors v (toVector m)
+
+isInMatrix :: Int -> (Int, Int) -> Bool
+isInMatrix s (x, y)
+  | x < 0 || x >= s = False
+  | y < 0 || y >= s = False
+  | otherwise = True
+
+findAll9 :: [[Char]] -> [(Int, (Int, Int))]
+findAll9 m =
+  helper m (0, 0)
+  where
+    helper matrix (x, y)
+      | y >= length matrix = []
+      | x >= length matrix = helper matrix (0, y + 1)
+      | otherwise =
+          if matrix !! y !! x == '9'
+            then (9, (x, y)) : helper matrix (x + 1, y)
+            else helper matrix (x + 1, y)
+
+process9 :: [[Char]] -> Int -> (Int, (Int, Int)) -> [(Int, Int)] -> [[(Int, Int)]]
+process9 m ms t p =
+  let up = sumVectorMovement (snd t) Up
+      down = sumVectorMovement (snd t) Down
+      left = sumVectorMovement (snd t) Left
+      right = sumVectorMovement (snd t) Right
+      x = fst (snd t)
+      y = snd (snd t)
+      digit = fst t
+   in ( if not (isInMatrix ms (snd t)) || (digit /= digitToInt (m !! y !! x))
+          then []
+          else
+            ( if fst t == 0
+                then [reverse (snd t : p)]
+                else
+                  process9 m ms (digit - 1, up) (snd t : p)
+                    ++ process9 m ms (digit - 1, down) (snd t : p)
+                    ++ process9 m ms (digit - 1, left) (snd t : p)
+                    ++ process9 m ms (digit - 1, right) (snd t : p)
+            )
+      )
+
+{- if isInMatrix ms up && digitToInt (m !! snd up !! fst up) == (fst t - 1)
+     then if fst t == 1 then up else process9 m ms (fst t - 1, up)
+     else
+       if isInMatrix ms down && digitToInt (m !! snd down !! fst down) == (fst t - 1)
+         then if fst t == 1 then down else process9 m ms (fst t - 1, down)
+         else
+           if isInMatrix ms left && digitToInt (m !! snd left !! fst left) == (fst t - 1)
+             then if fst t == 1 then left else process9 m ms (fst t - 1, left)
+             else
+               if isInMatrix ms right && digitToInt (m !! snd right !! fst right) == (fst t - 1)
+                 then if fst t == 1 then right else process9 m ms (fst t - 1, right)
+                 else (-1, -1) -}
+
+process9s :: [[Char]] -> Int -> [(Int, (Int, Int))] -> [[(Int, Int)]]
+process9s _ _ [] = []
+process9s m ms (el : tl) =
+  process9 m ms el [] ++ process9s m ms tl
+
+headTl :: [a] -> [a]
+headTl [] = []
+headTl l = [head l, last l]
+
+firstPart :: FilePath -> IO ()
+firstPart filename = do
+  contents <- readFile filename
+  let ls = lines contents
+  let matrixSize = length ls
+  let lists = process9s ls matrixSize (findAll9 ls)
+  print $ length $ fromList $ map headTl (filter (\el -> length el == 10) lists)
+
+secondPart :: FilePath -> IO ()
+secondPart filename = do
+  contents <- readFile filename
+  let ls = lines contents
+  let matrixSize = length ls
+  let lists = process9s ls matrixSize (findAll9 ls)
+  print $ length $ fromList $ filter (\el -> length el == 10) lists
