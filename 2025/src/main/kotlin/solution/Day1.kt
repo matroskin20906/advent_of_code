d0a92f354d1a71c95f246e48fdc54837bc55cca8
Aleksandr Matrosov <matroskin20906@gmail.com>
2025-12-01 14:06:29 +0200

    feat: day 1 part 2.

--- 2025/src/main/kotlin/solution/Day1.kt
+++ 2025/src/main/kotlin/solution/Day1.kt
@@ -28,6 +28,30 @@ class Day1 : Solution {
     }
 
     override fun partTwo(lines : List<String>): String {
-        TODO("Not yet implemented")
+        for (line in lines) {
+            val pair = parseLine(line)
+            val direction = pair.first
+            var step = pair.second
+            if (step > 100) {
+                password += step / 100
+                step %= 100
+            }
+            position = when (direction) {
+                Direction.LEFT -> {
+                    if (position != 0 && position - step < 0)
+                        password++
+                    (100 + position - step) % 100
+                }
+                Direction.RIGHT -> {
+                    if (position != 0 && position + step > 100)
+                        password++
+                    (position + step) % 100
+                }
+            }
+            if (position == 0)
+                password++
+        }
+
+        return password.toString()
     }
 }
