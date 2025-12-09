43cd0ca8c8694464f4a307be8cc0b48c6df6ed41
Aleksandr Matrosov <matroskin20906@gmail.com>
2025-12-09 18:24:25 +0200

    feat: day 9 clean up.

--- 2025/src/main/kotlin/stuff/Polygon.kt
+++ 2025/src/main/kotlin/stuff/Polygon.kt
@@ -42,31 +42,6 @@ class Polygon {
         return count % 2 == 1
     }
 
-    private fun orientation(a: Coordinate, b: Coordinate, c: Coordinate): Int {
-        val v = (b.y - a.y) * (c.x - b.x) - (b.x - a.x) * (c.y - b.y)
-        return when {
-            v > 0 -> 1
-            v < 0 -> -1
-            else -> 0
-        }
-    }
-
-    private fun segmentsIntersect(a: Coordinate, b: Coordinate, c: Coordinate, d: Coordinate): Boolean {
-        val o1 = orientation(a, b, c)
-        val o2 = orientation(a, b, d)
-        val o3 = orientation(c, d, a)
-        val o4 = orientation(c, d, b)
-
-        if (o1 != o2 && o3 != o4) return true
-
-        if (o1 == 0 && isPointOnSegment(c, a, b)) return true
-        if (o2 == 0 && isPointOnSegment(d, a, b)) return true
-        if (o3 == 0 && isPointOnSegment(a, c, d)) return true
-        if (o4 == 0 && isPointOnSegment(b, c, d)) return true
-
-        return false
-    }
-
     fun isSegmentInside(a: Coordinate, b: Coordinate): Boolean {
         if (!isPointInside(a)) return false
         if (!isPointInside(b)) return false
@@ -86,34 +61,6 @@ class Polygon {
         }
 
         return true
-        /*
-        for (i in vertices.indices) {
-            val c = vertices[i]
-            val d = vertices[(i + 1) % vertices.size]
-
-            if (segmentsIntersect(a, b, c, d)) {
-                val touching = isPointOnSegment(a, c, d)
-                        || isPointOnSegment(b, c, d)
-                if (!touching) {
-                    if (a.x == b.x) {
-                        for (y in c.y..max(a.y, b.y)) {
-                            if (!isPointInside(Coordinate(a.x, y))) {
-                                return false
-                            }
-                        }
-                    } else if (a.y == b.y) {
-                        for (x in c.x..max(a.x, b.x)) {
-                            if (!isPointInside(Coordinate(x, a.y))) {
-                                return false
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return true
-         */
     }
 
     override fun toString(): String {
